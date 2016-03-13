691da10a5f28d97109ea5f338ad02a3b919a2f23
Víctor Pavón <victormpav@gmail.com>
2016-03-13 15:18:37 +0100

    Added new icon

--- HookReviewUi/ReviewCreationDlg.Designer.cs
+++ HookReviewUi/ReviewCreationDlg.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReviewCreationDlg));
             this.reviewMessageTextBox = new System.Windows.Forms.TextBox();
             this.reviewMessageLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
@@ -120,6 +121,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(575, 274);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ReviewCreationDlg";
             this.Text = "Review creation";
             this.tableLayoutPanel1.ResumeLayout(false);
